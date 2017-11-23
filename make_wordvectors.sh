a7deae9e8690ebf1f7665cbe6f538c799c22f9f0
matstos <matstos@stud.ntnu.no>
2017-11-23 11:12:48 +0100

    added newer wiki to script

--- make_wordvectors.sh
+++ make_wordvectors.sh
@@ -6,18 +6,18 @@ lcode="xx" # ISO 639-1 code of target language. See `lcodes.txt`.
 max_corpus_size=1000000000 # the maximum size of the corpus. Feel free to adjust it according to your computing power.
 vector_size=300 # the size of a word vector
 window_size=5 # the maximum distance between the current and predicted word within a sentence.
-vocab_size=20000 # the maximum vocabulary size
+vocab_size=50250 # the maximum vocabulary size
 num_negative=5 # the int for negative specifies how many “noise words” should be drawn
 ############## END #####################
 
-echo "step 0. Make `data` directory and move there.`
+echo "step 0. Make `data` directory and move there."
 mkdir data; cd data
 
 echo "step 1. Download the stored wikipedia file to your disk."
-wget "https://dumps.wikimedia.org/${lcode}wiki/20161201/${lcode}wiki-20161201-pages-articles-multistream.xml.bz2"
+wget "https://dumps.wikimedia.org/${lcode}wiki/20171103/${lcode}wiki-20171103-pages-articles-multistream.xml.bz2"
 
 echo "step 2. Extract the bz2 file."
-bzip2 -d "${lcode}wiki-20161201-pages-articles-multistream.xml.bz2"
+bzip2 -d "${lcode}wiki-20171103-pages-articles-multistream.xml.bz2"
 
 cd ..
 echo "step 3. Build Corpus."
